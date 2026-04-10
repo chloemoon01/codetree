fa953abcdbe734313ad7b552eee987efd11f0261
chloemoon01 <yeongeun2020@gmail.com>
2026-04-10 21:06:49 +0900

    Title: 1-9 개수 세기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260410/1-9 개수 세기/count-one-to-nine.py
@@ -0,0 +1,41 @@
+N = int(input())
+arr = list(map(int,input().split()))
+
+one=0
+two=0
+three=0
+four=0
+five=0
+six=0
+seven=0
+eight=0
+nine=0
+
+for i in range(len(arr)):
+    if arr[i]==1:
+        one+=1
+    if arr[i]==2:
+        two+=1
+    if arr[i]==3:
+        three+=1
+    if arr[i]==4:
+        four+=1
+    if arr[i]==5:
+        five+=1
+    if arr[i]==6:
+        six+=1
+    if arr[i]==7:
+        seven+=1
+    if arr[i]==8:
+        eight+=1
+    if arr[i]==9:
+        nine+=1
+print(one)
+print(one)
+print(one)
+print(one)
+print(one)
+
+
+
+    
